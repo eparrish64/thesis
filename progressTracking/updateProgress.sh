1aa699e86c1ec6e3f1905e3d72958f3a5620427f
Tyler Burch <burcht11@gmail.com>
2019-01-08 21:48:48 -0600

    Add word count for unix

--- progressTracking/updateProgress.sh
+++ progressTracking/updateProgress.sh
@@ -4,6 +4,9 @@ echo "Updating dissertation progress"
 DOCUMENT='/Users/tburch/Documents/gitDevelopment/thesis/mythesis.pdf'
 PROGRESSFILE='/Users/tburch/Documents/gitDevelopment/thesis/progressTracking/progress.csv'
 WORDCOUNT=`ps2ascii ${DOCUMENT} | wc -w`
+# Use this line in OSX
 PAGECOUNT=`mdls -name kMDItemNumberOfPages -raw ${DOCUMENT}`
+# Use this line in Linux
+# PAGECOUNT=`pdfinfo mythesis.pdf | grep Pages | sed 's/[^0-9]*//'`
 echo `date '+%Y-%m-%d %H:%M:%S'`,$WORDCOUNT,$PAGECOUNT >> $PROGRESSFILE
 echo "Done! Page count ${PAGECOUNT}, word count ${WORDCOUNT}"
