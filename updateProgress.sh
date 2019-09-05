aacf236de66f9de7c9a91c78872367dfab474fe4
Tyler Burch <burcht11@gmail.com>
2019-09-05 12:41:07 -0500

    fix issue in texinfo

--- updateProgress.sh
+++ updateProgress.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/bin:/Library/TeX/texbin/:$PATH
+#export PATH=/usr/local/bin:$PATH
 echo "Updating dissertation progress"
 DOCUMENT='/Users/tburch/Documents/gitDevelopment/thesis/thesisBurch.pdf'
 TEX_DOC='/Users/tburch/Documents/gitDevelopment/thesis/thesisBurch.tex'
 PROGRESSFILE='/Users/tburch/Documents/gitDevelopment/thesis/progressTracking/progress.csv'
+cd /Users/tburch/Documents/gitDevelopment/thesis/
 WORDCOUNT=`texcount -sum -total -merge {$TEX_DOC} | grep "Sum count:" | tr -d "Sum count: "`
 # Use this line in OSX
 PAGECOUNT=`mdls -name kMDItemNumberOfPages -raw ${DOCUMENT}`
@@ -11,3 +13,5 @@ PAGECOUNT=`mdls -name kMDItemNumberOfPages -raw ${DOCUMENT}`
 # PAGECOUNT=`pdfinfo mythesis.pdf | grep Pages | sed 's/[^0-9]*//'`
 echo `date '+%Y-%m-%d %H:%M:%S'`,$WORDCOUNT,$PAGECOUNT >> $PROGRESSFILE
 echo "Done! Page count ${PAGECOUNT}, word count ${WORDCOUNT}"
+
+python /Users/tburch/Documents/gitDevelopment/thesis/progressTracking/plotProgress.py
